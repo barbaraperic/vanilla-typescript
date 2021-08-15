257f88da91ba2af66bb94dc4596ff3345d81a98e
Barbara Peric <b.peric10@gmail.com>
2021-08-15 17:20:18 +0200

    types

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
 let fruitName = "banana"
 
 let array = ["banana", 24]
+
+
+// TYPES IN JAVASCRIPT
+
+// 6 primitive types - immutable
+// 1. boolean is true of false (0 and '' are falsy)
+// 2. NaN - number that shouldn't exist
+// 3. strings 
+// 4. all symbols are unique, you can give them description
+// 5. undefined is uninitialized value
+// 6. null is absent value
+
+
+// structural types
+// 1. object, any number of properties that point to a value
+// 2. arrays, data is ordered 
+// 3. functions are defined blocks of code
+// 4. classes are objects, constructor function 
